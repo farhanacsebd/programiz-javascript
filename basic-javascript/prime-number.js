5420e8e30fa4799b7417963b0bfce30440f08fd4
farhanacsebd <farhana.cse.bd@gmail.com>
2022-08-21 23:50:58 +0600

    find prime number

---
+++ basic-javascript/prime-number.js
@@ -0,0 +1,7 @@
+const number =57;
+if(Math.sqrt(number) >= 1 && Math.sqrt(number) != 0 && number % 2 != 0 && number % 3 != 0 && number % 5 != 0){
+    console.log('that is prime number');
+}
+else{
+    console.log('no prime number');
+}
