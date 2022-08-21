5fe5896cd61e3974fc1118a1b44eb6a97b07daf5
farhanacsebd <farhana.cse.bd@gmail.com>
2022-08-21 23:18:31 +0600

    checking the number

---
+++ basic-javascript/check-number.js
@@ -0,0 +1,11 @@
+const number = -9;
+if(number > 0){
+    console.log('that is positive number.');
+}
+else if(number == 0){
+    console.log('that is zero.');
+}
+
+else{
+    console.log('that is nagative number.');
+}
